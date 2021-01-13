93488d36097db50b819b70a75de3bedb18c01c7b
hamengde <76994773+hamengde@users.noreply.github.com>
2021-01-13 19:55:08 +0800

    Update ShopDAL.cs
    ok

--- Shopping_Web_Api/Shopping_DAL/ShopDAL.cs
+++ Shopping_Web_Api/Shopping_DAL/ShopDAL.cs
@@ -4,10 +4,6 @@ namespace Shopping_DAL
 {
     public class ShopDAL
     {
-        public void Show()
-        {
-            var i = "测试数据";
-            var row = "Test2";
-        }
+       
     }
 }
